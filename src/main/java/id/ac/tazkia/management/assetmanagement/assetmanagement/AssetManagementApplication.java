1ff9e6a1b1fe541b39bb03d3a5edfe10a42b0f9c
gifar <gifar@tazkia.ac.id>
2018-11-26 09:41:15 +0700

    started project

---
+++ src/main/java/id/ac/tazkia/management/assetmanagement/assetmanagement/AssetManagementApplication.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.management.assetmanagement.assetmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetManagementApplication.class, args);
+	}
+}
